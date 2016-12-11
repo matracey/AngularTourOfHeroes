1dbf1578fb86c20cc262eb3147932988116382f7
Martin Tracey <matracey@outlook.ie>
2016-12-11 03:24:47 +0000

    Added dashboard, routing and styling.

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,17 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component'
 import { HeroService } from './hero.service';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent
   ],
   providers: [
     HeroService
