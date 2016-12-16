4fbbfd2716b21a850415ebd30eeff81a8c8899ed
Martin Tracey <matracey@outlook.ie>
2016-12-16 01:02:25 +0000

    Continued adding bootstrap 4

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -21,18 +21,17 @@ import { HeroService } from './hero.service';
 @Component({
     moduleId: module.id,
     selector: 'my-hero-detail',
-    templateUrl: 'hero-detail.component.html',
-    styleUrls: ['hero-detail.component.css']
+    templateUrl: 'hero-detail.component.html'
 })
 export class HeroDetailComponent implements OnInit {
     @Input()
     hero: Hero;
 
     constructor(
-        private heroService: HeroService, 
-        private route: ActivatedRoute, 
+        private heroService: HeroService,
+        private route: ActivatedRoute,
         private location: Location
-    ){
+    ) {
 
     }
 
